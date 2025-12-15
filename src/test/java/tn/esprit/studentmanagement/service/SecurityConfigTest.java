8e562e8920cd63162ae0eacd992579f338ec384c
fatma-benkhedher <fatma.benkhedher@esprit.tn>
2025-12-15 09:26:02 +0100

    last push

--- src/test/java/tn/esprit/studentmanagement/service/SecurityConfigTest.java
+++ src/test/java/tn/esprit/studentmanagement/service/SecurityConfigTest.java
@@ -2,12 +2,17 @@ package tn.esprit.studentmanagement.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class SecurityConfigTest {
 
     @Test
     void contextLoads() {
-        // This test loads the Spring context including SecurityConfig
-        // SonarQube will count this as code coverage
+        // Loads Spring context including SecurityConfig
     }
 }
